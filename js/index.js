f3534cd1d7367a26ad812938d737e1f5cfb75f27
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-05 15:05:44 -0400

    added dblclick event on img and click event on links

--- js/index.js
+++ js/index.js
@@ -10,3 +10,30 @@ siteTitle.addEventListener("mouseleave", ()=>{
     siteTitle.style.fontSize = "4rem";
     siteTitle.style.color = "black";
 });
+
+const headerImg = document.querySelector("header.intro > img");
+
+headerImg.addEventListener("dblclick", ()=>{
+    const input = window.prompt("Caption this image!");
+    if(document.querySelector("p.header-img-caption")){
+        const caption = document.querySelector("p.header-img-caption");
+        caption.textContent = input;
+    }else {
+        const caption = document.createElement("p");
+        caption.classList.add("header-img-caption");
+        caption.textContent = input;
+        caption.style.textAlign = "center";
+
+        headerImg.parentNode.insertBefore(caption, headerImg.nextSibling);
+    }
+});
+
+const links = document.querySelectorAll("a");
+
+links.forEach(link=>{
+    link.addEventListener("click", e =>{
+        e.preventDefault();
+        window.alert("Woah! You clicked a link!")
+    });
+});
+
