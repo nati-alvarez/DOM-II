6688fa9bb08143071de277e181ef72f5f5bca155
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-05 05:54:59 -0400

    Added mouseleave event on site title in nav

--- js/index.js
+++ js/index.js
@@ -5,3 +5,8 @@ siteTitle.addEventListener('mouseenter',()=>{
     siteTitle.style.fontSize = "5rem";
     siteTitle.style.color = "coral";
 });
+
+siteTitle.addEventListener("mouseleave", ()=>{
+    siteTitle.style.fontSize = "4rem";
+    siteTitle.style.color = "black";
+});
