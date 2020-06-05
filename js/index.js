0033e6b149eee59246e49be18dd894396b7958d1
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-05 05:53:42 -0400

    Added mouseenter event on site title in nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const siteTitle = document.querySelector("h1.logo-heading");
+
+siteTitle.addEventListener('mouseenter',()=>{
+    siteTitle.style.fontSize = "5rem";
+    siteTitle.style.color = "coral";
+});
